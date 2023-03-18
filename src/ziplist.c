920dd69e7c371c3747f64313358fd3a6ba8ba75b
impact-eintr <yixingwei4@gmail.com>
2023-03-18 18:08:40 +0800

    ziplist 计算插入位置

--- src/ziplist.c
+++ src/ziplist.c
@@ -304,6 +304,36 @@ static int zipTryEncoding(unsigned char *entry, unsigned int entrylen, long long
   return 0; // 转换失败
 }
 
+static void zipSaveInteger(unsigned char*p, int64_t value, unsigned char encoding) {
+  int16_t i16;
+  int32_t i32;
+  int64_t i64;
+
+  if (encoding == ZIP_INT_8B) {
+    ((int8_t*)p)[0] = (int8_t)value;
+  } else if (encoding == ZIP_INT_16B) {
+    i16 = value;
+    memcpy(p, &i16, sizeof(i16));
+    memrev16ifbe(p);
+  } else if (encoding == ZIP_INT_24B) {
+    i32 = value<<8;
+    memrev32ifbe(&i32);
+    memcpy(p, ((uint8_t*)&i32)+1, sizeof(i32)-sizeof(uint8_t));
+  } else if (encoding == ZIP_INT_32B) {
+    i32 = value;
+    memcpy(p, &i32, sizeof(i32));
+    memrev32ifbe(p);
+  } else if (encoding == ZIP_INT_64B) {
+    i64 = value;
+    memcpy(p, &i64, sizeof(i64));
+    memrev64ifbe(p);
+  } else if (encoding >= ZIP_INT_IMM_MIN && encoding <= ZIP_INT_IMM_MAX) {
+    // Nothing to do
+  } else {
+    assert(NULL);
+  }
+}
+
 
 // 将p所指向的列表节点的信息全部保存在 zlentry 中 并返回该 zlentry
 static zlentry zipEntry(unsigned char *p) {
@@ -334,7 +364,7 @@ unsigned char *ziplistNew(void) {
 
 // 调整尺寸
 static unsigned char *ziplistResize(unsigned char *zl, unsigned int len) {
-  printf("扩展长度: %d", len);
+  printf("扩展长度: %d\n", len);
   zl = zrealloc(zl, len);
   ZIPLIST_BYTES(zl) = intrev32ifbe(len);
   zl[len-1] = ZIP_END;
@@ -380,15 +410,28 @@ static unsigned char *__ziplistInsert(unsigned char *zl, unsigned char *p, unsig
   nextdiff = (p[0] != ZIP_END) ? zipPrevLenByteDiff(p, reqlen) : 0;
 
   printf("reqlen %ld %d\n", reqlen, nextdiff);
-  offset = p-zl;
+  offset = p-zl; // /插入位置与表头的偏移
   zl = ziplistResize(zl, curlen+reqlen+nextdiff);
-  p = zl+offset;
+  p = zl+offset; // 找到新的插入位置
 
+  if (p[0] != ZIP_END) { // 新元素后还有节点
+    memmove(p+reqlen, p-nextdiff, curlen-offset+nextdiff);
+    zipPrevEncodeLength(p+reqlen, prevlen);
+    ZIPLIST_TAIL_OFFSET(zl) = intrev32ifbe(intrev32ifbe(ZIPLIST_TAIL_OFFSET(zl))+reqlen);
+    tail = zipEntry(p+reqlen);
+    if (p[reqlen + tail.headersize + tail.len] != ZIP_END) {
+      ZIPLIST_TAIL_OFFSET(zl) =
+          intrev32ifbe(intrev32ifbe(ZIPLIST_TAIL_OFFSET(zl)) + nextdiff);
+    }
+  } else {
+    // 最后一个节点到表头的距离
+    ZIPLIST_TAIL_OFFSET(zl) = intrev32ifbe(p - zl); //
+  }
 
   if (ZIP_IS_STR(encoding)) {
     memcpy(p, s, slen);
   } else {
-    //zipSaveInteger(p, value, encoding);
+    zipSaveInteger(p, value, encoding);
   }
 
   ZIPLIST_INCR_LENGTH(zl, 1);
